7167c149864842d6ac10d346eeae48791174fcab
kobdash <jefftny1@gmail.com>
2023-09-19 04:19:26 +0200

    first commit

---
+++ src/main/java/com/mycompany/todolistapp/ToDoListApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.todolistapp;
+
+/**
+ *
+ * @author jeff
+ */
+public class ToDoListApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
