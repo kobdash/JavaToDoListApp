0173ef5c6c3aa2dc89000bc8c16ad91455165e5a
kobdash <jefftny1@gmail.com>
2023-09-27 21:16:42 +0200

    first commit

--- src/main/java/com/mycompany/todolistapp/ToDoListApp.java
+++ src/main/java/com/mycompany/todolistapp/ToDoListApp.java
@@ -1,16 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.todolistapp;
 
+import ToDoListAppGUI.ToDoListAppGUI; // Import your GUI class
+
 /**
- *
- * @author jeff
+ * Main class for your To-Do List Application.
  */
 public class ToDoListApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Create an instance of your GUI class
+        ToDoListAppGUI guiApp = new ToDoListAppGUI();
+
+        // Set the behavior for when the GUI window is closed
+        guiApp.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+
+        // Make the GUI window visible
+        guiApp.setVisible(true);
     }
 }
